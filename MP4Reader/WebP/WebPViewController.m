f8f4263ed9a5cc09a61c20a18436ac58f9384703
肖学源 <xiaoxueyuan1@huya.com>
2019-11-14 19:44:51 +0800

    添加gif webp 方式

--- MP4Reader/WebP/WebPViewController.m
+++ MP4Reader/WebP/WebPViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "WebPViewController.h"
+#import <YYImage.h>
 
 @interface WebPViewController ()
+
+@property (weak, nonatomic) IBOutlet YYAnimatedImageView *imageView;
 
 @end
 
@@ -19,14 +22,9 @@
     // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)playAction:(id)sender {
+    YYImage *image = [YYImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"000_iSpt.webp" ofType:nil]];
+    self.imageView.image = image;
 }
-*/
 
 @end
